9ea409584cb994f5bf12fa2b2d318c655215d1de
Ahmed Al-Obaidy <ahmed@A.local>
2019-07-18 16:01:42 +0200

    services

--- resources/views/services/search.blade.php
+++ resources/views/services/search.blade.php
@@ -1,12 +1,44 @@
 @extends('layouts.ineed')
 {{-- The list of available providers in a category --}}
 @section('content')
+<head>
+    <style>
+        .service {
+            text-align: center;
+            width: 200px;
+            margin-top: 50px;
+            margin-left: auto;
+            margin-right: auto;
+            font-size: 35px;
+            background-color: white;
+            border-radius: 20px;
+            
+        }
+        .container {
+            display: flex;
+            flex-direction: column;
+            background-color: rgba(255, 255, 255, 0.805);
+            margin-top: 25px;
+            margin-bottom: 50px;
+            margin-left: auto;
+            margin-right: auto;
+            width: 60%;
+            font-size: 20px;
+            height: 100px;
+            border-radius: 10px;
+            text-decoration: none;
+        }
+        .container>a{
+            text-decoration: none;
+        }
+    </style>
+</head>
     
-    <div>{{ $serviceObject->name }}s</div>
+    <div class="service">{{ $serviceObject->name }}s</div>
 
     <div>
         @foreach($providers as $provider)
-            <div>
+            <div class="container">
                 <a href="{{ action('DisplayProfileController@index', $provider->id) }}">
                 <div>{{ $provider->name}}</div>
                 <div>{{ $provider->description}}</div>
